6efce7fcefa7c96a23cdc8b95575faf0b8376942
Feer-trujillo <mf_te@hotmail.com>
2024-08-01 20:34:12 -0600

    Updated

--- practicalogic.js
+++ practicalogic.js
@@ -1,26 +1,27 @@
-// Solicitar el nombre de usuario
+
+//**Perfil del usuario */
 const username = prompt("¿Cuál es tu nombre de usuario?");
-
-// Solicitar la edad del usuario
 const age = prompt("¿Cuántos años tienes?");
-
-// Solicitar la lista de películas favoritas
-// Nota: Se espera que el usuario ingrese las películas separadas por comas
 const moviesInput = prompt("Ingresa tus películas favoritas, separadas por comas");
-
-// Convertir el string de películas en un array
 const movies = moviesInput.split(',').map(movie => movie.trim());
 
-// Función para mostrar los datos en la consola
-function displayUserInfo(name, age, movieList) {
+
+function displayUserInfo(name, age, movieList) {//**// Función para mostrar los datos en la consola */
     console.log(`Nombre de usuario: ${name}`);
     console.log(`Edad: ${age} años`);
-    
-    // Mostrar un mensaje para cada película favorita
+
     movieList.forEach(movie => {
         console.log(`La película "${movie}" es una de mis favoritas.`);
     });
 }
 
-// Llamar a la función con los datos del usuario
-displayUserInfo(username, age, movies);
+displayUserInfo(username, age, movies);//**// Llamar a la función con los datos del usuario */
+
+
+//**Número mas alto */
+
+let numbers=[];
+ for (let i= 0; i< 10; i++) {
+    let num = parseFloat(prompt(`Introduce el número ${i + 1} de 10:`));
+    
+ }
